86517d6d01384897f8385f13e5f4f7e4defa5b27
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-07 11:55:55 -0400

    Update hcp_vault.tf

--- hcp_vault.tf
+++ hcp_vault.tf
@@ -19,3 +19,15 @@ output "hcp_vault_cluster_admin_token" {
   value = hcp_vault_cluster_admin_token.example.token
   sensitive  = true
 }
+
+
+resource "vault_ldap_auth_backend" "ldap" {
+    path        = "ldap"
+    url         =  aws_directory_service_directory.example.access_url
+    userdn      = "OU=Users,OU=Accounts,DC=example,DC=org"
+    userattr    = "sAMAccountName"
+    upndomain   = "EXAMPLE.ORG"
+    discoverdn  = false
+    groupdn     = "OU=Groups,DC=example,DC=org"
+    groupfilter = "(&(objectClass=group)(member:1.2.840.113556.1.4.1941:={{.UserDN}}))"
+}
