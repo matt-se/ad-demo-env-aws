e13faba6cfb4b8166df64de7ceead3ad0ed52edd
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-07 11:42:09 -0400

    Update hcp_vault.tf

--- hcp_vault.tf
+++ hcp_vault.tf
@@ -3,4 +3,17 @@ resource "hcp_vault_cluster" "example" {
   cluster_id = "${var.environment_name}-vault-cluster"
   hvn_id     = hcp_hvn.hvn.hvn_id
   tier       = "dev"
+  public_endpoint = true
+}
+
+output "hcp_valut_public_url" {
+  value = hcp_vault_cluster.example.public_endpoint
+}
+
+resource "hcp_vault_cluster_admin_token" "example" {
+  cluster_id = "test-vault-cluster"
+}
+
+output "hcp_vault_cluster_admin_token" {
+  value = hcp_vault_cluster_admin_token.example.token
 }
