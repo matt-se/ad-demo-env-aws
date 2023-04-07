d5fa88de508a6ae385a1292a608b5a4b7404495c
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-07 13:22:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,6 @@ provider "hcp" {}
 
 provider "vault" {
   #token = hcp_vault_cluster_admin_token.example.token
-  #address = hcp_vault_cluster.example.public_endpoint
+  address = hcp_vault_cluster.example.public_endpoint
   namespace = "admin"
 }
