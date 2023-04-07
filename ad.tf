ea1da21244cc075c4959914f80a40244abd50ad8
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-07 11:36:03 -0400

    Update ad.tf

--- ad.tf
+++ ad.tf
@@ -13,3 +13,23 @@ resource "aws_directory_service_directory" "ad" {
     Project = "${var.environment_name}-ad"
   }
 }
+
+output "ad_dns_ip" {
+  value = aws_directory_service_directory.ad.dns_ip_addresses
+}
+
+output "ad_id" {
+  value = aws_directory_service_directory.ad.id
+}
+
+output "ad_name" {
+  value = aws_directory_service_directory.ad.name
+}
+
+output "ad_sg" {
+  value = aws_directory_service_directory.ad.security_group_id
+}
+
+output "ad_url" {
+  value = aws_directory_service_directory.ad.access_url
+}
