1787cd7dfd03d88fda10170e67460f18efe682a1
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-07 09:41:05 -0400

    1

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable aws_vpc_cidr {
+  description = "CIDR block for the VPC"
+}
+
+variable tfc_org {
+  default = "mattygrecgrec"
+}
+
+variable tfc_workspace {
+  default = "ad-demo-env-aws"
+}
