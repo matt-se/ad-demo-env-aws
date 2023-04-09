5bb392ba88198182eec3ff11fe63fdff39f28003
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-08 20:50:31 -0400

    user_data

--- ec2_dc.tf
+++ ec2_dc.tf
@@ -14,6 +14,11 @@ resource "aws_key_pair" "key_for_windows_server" {
 }
 
 
+data "template_file" "user_data" {
+  template = "${file("installAD.ps1")}"
+}
+
+
 resource "aws_instance" "windows" {
   ami           = data.aws_ami.windows-2022.id
   instance_type = "t2.micro"
@@ -41,36 +46,7 @@ resource "aws_instance" "windows" {
       agent      = false
     } */
 
-  user_data = <<-EOF
-    <powershell>
-    # Install the necessary AD DS role and management tools
-    Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-    
-    # Set up the domain and configure the AD Forest
-    $domainName = "${var.ad_domain_name}}"
-    $netBIOSName = "${var.ad_netBIOS_name}}"
-    $safeModePassword = "${var.ad_admin_password}}"
-    
-    # Convert the Safe Mode password to a Secure String
-    $secureSafeModePassword = ConvertTo-SecureString $safeModePassword -AsPlainText -Force
-    
-    # Install the new Forest with the specified domain name and Safe Mode password
-    Install-ADDSForest `
-      -CreateDnsDelegation:$false `
-      -DatabasePath "C:\Windows\NTDS" `
-      -DomainMode "Win2012R2" `
-      -DomainName $domainName `
-      -DomainNetbiosName $netBIOSName `
-      -ForestMode "Win2012R2" `
-      -InstallDns:$true `
-      -LogPath "C:\Windows\NTDS" `
-      -NoRebootOnCompletion:$false `
-      -SysvolPath "C:\Windows\SYSVOL" `
-      -Force:$true `
-      -SafeModeAdministratorPassword $secureSafeModePassword
-    </powershell>
-  EOF
-}
+  user_data = "${file(installAD.txt)}"
 
 output "windows_public_ip" {
   value = aws_instance.windows.public_ip
