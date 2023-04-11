423eb54da89271f67b7101c58518d762c82fa6a9
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-11 14:41:59 -0400

    Update vars.tf

--- AD/vars.tf
+++ AD/vars.tf
@@ -2,13 +2,13 @@ variable ad_controller_public_ip {
   description = "The public IP address of the AD controller"
   type        = string
 }
-
+/*
 variable ad_domain_name {
   description = "The domain name of the AD controller"
   type        = string
   default     = "mattygrecgrec.com"
 }
-
+*/
 variable ad_domain {
   description = "The domain of the AD controller"
   type        = string
