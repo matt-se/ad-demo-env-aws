efae599e9e084d3a1111dda7469c8d8d8709191f
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-11 14:56:38 -0400

    Update vault.tf

--- AD/vault.tf
+++ AD/vault.tf
@@ -1,6 +1,6 @@
 resource "vault_ldap_auth_backend" "ldap" {
     path        = "ldap"
-    url         =  var.ad_controller_public_ip
+    url         =  "ldap://${var.ad_controller_public_ip}"
     userdn      = "OU=Users,OU=Accounts,DC=${var.ad_domain},DC=com"
     userattr    = "sAMAccountName"
     upndomain   = "${var.ad_domain}.com"
