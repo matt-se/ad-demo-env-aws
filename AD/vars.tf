3644e0da220aec08f35328c12c4fc69763ca4f9e
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-11 12:32:18 -0400

    Update vars.tf

--- AD/vars.tf
+++ AD/vars.tf
@@ -7,4 +7,5 @@ variable ad_controller_public_ip {
 variable windows_password {
   description = "The password for the Windows VM.  You'll need to get this from the AWS portal."
   type        = string
+  default     = "q$80@?8$j9oCW=&A8X!ZtFAnj)Acat&z"
 }
