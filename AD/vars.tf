42dbddc9f4be6c725ab4341f195d7707381a3438
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-11 12:15:39 -0400

    Update vars.tf

--- AD/vars.tf
+++ AD/vars.tf
@@ -1,5 +1,11 @@
-var ad_controller_public_ip {
+variable ad_controller_public_ip {
   description = "The public IP address of the AD controller"
   type        = string
   default     = "1.1.1.1"
+}
+
+
+variable windows_password {
+  description = "The password for the Windows VM.  You'll need to get this from the AWS portal."
+  type        = string
 }
