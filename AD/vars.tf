60a59e92fb358ebc0b2d5f6ece365da2366391b1
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-11 14:12:55 -0400

    Update vars.tf

--- AD/vars.tf
+++ AD/vars.tf
@@ -3,9 +3,15 @@ variable ad_controller_public_ip {
   type        = string
 }
 
+variable ad_domain_name {
+  description = "The domain name of the AD controller"
+  type        = string
+  default     = "mattygrecgrec.com"
+}
 
 variable windows_password {
   description = "The password for the Windows VM.  You'll need to get this from the AWS portal."
   type        = string
   default     = "q$80@?8$j9oCW=&A8X!ZtFAnj)Acat&z"
 }
+
