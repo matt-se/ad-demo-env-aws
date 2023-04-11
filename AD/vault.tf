69d74e164b595de0940c8b9f085644ea53e7a2c3
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-11 14:42:43 -0400

    Update vault.tf

--- AD/vault.tf
+++ AD/vault.tf
@@ -3,7 +3,7 @@ resource "vault_ldap_auth_backend" "ldap" {
     url         =  var.ad_controller_public_ip
     userdn      = "OU=Users,OU=Accounts,DC=${var.ad_domain},DC=com"
     userattr    = "sAMAccountName"
-    upndomain   = var.ad_domain_name
+    upndomain   = "${var.ad_domain_name}.com"
     discoverdn  = false
     groupdn     = "OU=Groups,DC=${var.ad_domain},DC=com"
     groupfilter = "(&(objectClass=group)(member:1.2.840.113556.1.4.1941:={{.UserDN}}))"
