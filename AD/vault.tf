cb863079aca0cfc156df434915ce9063917245f8
matt-se <101576447+matt-se@users.noreply.github.com>
2023-04-11 15:10:58 -0400

    Update vault.tf

--- AD/vault.tf
+++ AD/vault.tf
@@ -1,12 +1,18 @@
 resource "vault_ldap_auth_backend" "ldap" {
     path        = "ldap"
     url         =  "ldap://${var.ad_controller_public_ip}"
-    userdn      = "OU=Users,OU=Accounts,DC=${var.ad_domain},DC=com"
-    userattr    = "sAMAccountName"
-    upndomain   = "${var.ad_domain}.com"
-    discoverdn  = true
-    groupdn     = "OU=Groups,DC=${var.ad_domain},DC=com"
-    groupfilter = "(&(objectClass=group)(member:1.2.840.113556.1.4.1941:={{.UserDN}}))"
     insecure_tls = true
     description = "LDAP auth backend for ${var.ad_domain}.com"
+
+    binddn      = "CN=Administrator,CN=Users,DC=${var.ad_domain},DC=com"
+    bindpass    = var.windows_password
+    userdn      = "OU=Users,OU=Accounts,DC=${var.ad_domain},DC=com"
+    userattr    = "sAMAccountName"
+    userfilter  = "(objectClass=person)"
+    
+    #upndomain   = "${var.ad_domain}.com"
+    #discoverdn  = true
+    #groupdn     = "OU=Groups,DC=${var.ad_domain},DC=com"
+    #groupfilter = "(&(objectClass=group)(member:1.2.840.113556.1.4.1941:={{.UserDN}}))"
+    
 }
