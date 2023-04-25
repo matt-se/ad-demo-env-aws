731e2b014bfdef1e9a4a2d709eca4fe642106699
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-04-25 10:47:40 -0400

    Update vault.tf

--- AD/vault.tf
+++ AD/vault.tf
@@ -1,3 +1,4 @@
+### mount the LDAP auth backend
 resource "vault_ldap_auth_backend" "ldap" {
     path        = "ldap"
     url         =  "ldap://${var.ad_controller_public_ip}"
@@ -21,4 +22,52 @@ resource "vault_ldap_auth_backend" "ldap" {
 
 
     token_policies = ["default"]
+}
+
+### Create a group in this LDAP auth mount.  Any user who is a member of this group in AD will be added to this group when they log into Vault and will inherit the policies of this group.
+resource "vault_ldap_auth_backend_group" "z" {
+    # Because we're using memberOf, this should be the full DN of the group.
+    groupname = "cn=groupz,cn=users,dc=mattygrecgrec,dc=com"
+    policies  = ["groupz_kv_secrets"]
+    backend   = vault_ldap_auth_backend.ldap.path
+}
+
+
+### Create a custom kv2 secret mount for groupz
+resource "vault_mount" "kv2_for_groupz" {
+  path        = "groupz_kv_secrets"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 secret engine mount for AD security group groupz"
+}
+
+resource "vault_kv_secret_backend_v2" "example" {
+  mount                = vault_mount.kv2_for_groupz.path
+  max_versions         = 5
+  delete_version_after = 12600
+  cas_required         = false
+}
+
+resource "vault_kv_secret_v2" "example" {
+  mount                      = vault_mount.kv2_for_groupz.path
+  name                       = "secret"
+  cas                        = 1
+  delete_all_versions        = true
+  data_json                  = jsonencode(
+  {
+    zip       = "zap",
+    foo       = "bar"
+  }
+  )
+}
+
+### Create the policy that allows members of groupz to access the kv path
+resource "vault_policy" "groupz_kv_secrets" {
+  name = "groupz_kv_secrets"
+
+  policy = <<EOT
+path "groupz_kv_secrets/*" {
+  capabilities = ["create", "read", "update", "patch", "delete", "list"]
+}
+EOT
 }
